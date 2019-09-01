a927bbd41bd2abd5593dd58ac9141db0fa5ccfb4
Pi Moll <pi1402@gmail.com>
2019-09-01 14:28:07 +0200

    Reorganize GUI layout for poles to allow for row inserts.

--- gui/adjustmentDialog.py
+++ gui/adjustmentDialog.py
@@ -24,8 +24,10 @@ import os
 from .adjustmentPlot import AdjustmentPlot
 from .guiHelperFunctions import MyNavigationToolbar
 
-from qgis.PyQt.QtCore import QSize
-from qgis.PyQt.QtWidgets import QDialog, QSizePolicy, QDoubleSpinBox, QSpinBox, QPushButton, QLineEdit
+from qgis.PyQt.QtCore import QSize, Qt
+from qgis.PyQt.QtWidgets import (QDialog, QSizePolicy, QDoubleSpinBox,
+                                 QSpinBox, QPushButton, QLineEdit, QHBoxLayout,
+                                 QSpacerItem)
 from qgis.PyQt.QtGui import QIcon, QPixmap
 
 from .ui_adjustmentDialog import Ui_Dialog
@@ -90,89 +92,135 @@ class AdjustmentDialog(QDialog, Ui_Dialog):
     
     def addPolesToGui(self):
         [disp_data, di, seilDaten, HM, IS, projInfo, resultStatus, locPlot] = self.data
-        
+
         rangeBuffer = 10
         hDist = HM['idx']
         poleCount = len(hDist)
-        
-        # Ankerfeld links
-        self.poleListing[0] = {}
-        self.addPoleName(0, 'Verankerung')
-        self.addPoleHDist(0, -7.0, [-1 * rangeBuffer, 0])  # TODO: Wert aus IS auslesen
 
-        # Stützen
+        # Ankerfeld start
+        self.addAnker(0, -7.0, [-1 * rangeBuffer, 0])
+
+        # Poles
         for idx in range(poleCount):
-            # Create new Row and add gui elements
-            poleNr = idx + 1
-            self.poleListing[poleNr] = {}
-            
-            self.addPoleName(poleNr, f'{poleNr}. Stütze')
+            delBtn = False
+            addBtn = False
             lowerRange = hDist[idx - 1] if idx > 0 else 0
             upperRange = hDist[idx + 1] if idx < poleCount - 1 else hDist[idx] + rangeBuffer
-            self.addPoleHDist(poleNr, hDist[idx], [lowerRange, upperRange])
-            self.addPoleHeight(poleNr, HM['h'][idx])
-            self.addPoleAngle(poleNr, 0)
+
+            # Delete Buttons vor all but the first and last pole
             if idx != 0 and idx != poleCount - 1:
-                self.addPoleDel(poleNr)
-            
-        # Ankerfeld rechts
-        self.poleListing[poleCount + 1] = {}
-        self.addPoleName(poleCount + 1, 'Verankerung')
-        self.addPoleHDist(poleCount + 1, 327.0, [327.0, 327.0 + rangeBuffer]) # TODO
+                delBtn = True
+            # Add Button for all but the last pole
+            if idx != poleCount - 1:
+                addBtn = True
 
+            self.addPole(idx + 1, hDist[idx], [lowerRange, upperRange],
+                         HM['h'][idx], delBtn, addBtn)
 
-    def addPoleAddBtn(self):
-        pass
-    
-    def addPoleName(self, idx, value):
+        # Ankerfeld end
+        self.addAnker(poleCount + 1, 327.0, [327.0, 327.0 + rangeBuffer])
+
+    def addRow(self, idx):
+        rowLayout = QHBoxLayout()
+        rowLayout.setAlignment(Qt.AlignLeft)
+        # if last position in grid addLayout, else insertLayout
+        if self.poleVGrid.count() == idx + 1:
+            self.poleVGrid.addLayout(rowLayout)
+        else:
+            self.poleVGrid.insertLayout(idx, rowLayout)
+
+        self.poleListing[idx] = {}
+        return rowLayout
+
+    def addAnker(self, idx, dist, distRange):
+        row = self.addRow(idx)
+        self.addPoleAddBtn(False)
+        self.addPoleName(row, idx, 'Verankerung')
+        self.addPoleHDist(row, idx, dist, distRange)  # TODO: Wert aus IS auslesen
+
+    def addPole(self, poleNr, dist, distRange, height, delBtn, addBtn):
+        row = self.addRow(poleNr)
+        self.addPoleName(row, poleNr, f'{poleNr}. Stütze')
+        self.addPoleHDist(row, poleNr, dist, distRange)
+        self.addPoleHeight(row, poleNr, height)
+        self.addPoleAngle(row, poleNr, 0)
+
+        if delBtn:
+            self.addPoleDel(row, poleNr)
+        if addBtn:
+            self.addPoleAddBtn(poleNr)
+
+    def addPoleAddBtn(self, idx):
+        # Placeholder for add Button
+        if not idx:
+            placeholder = QSpacerItem(5, 31+20, QSizePolicy.Fixed,
+                                      QSizePolicy.Fixed)
+            self.addBtnVGrid.addItem(placeholder)
+            return
+
+        btn = QPushButton(self.tabPoles)
+        btn.setMaximumSize(QSize(19, 19))
+        btn.setText("")
+        icon = QIcon()
+        icon.addPixmap(
+            QPixmap(":/plugins/SeilaplanPlugin/gui/icons/icon_plus.png"),
+            QIcon.Normal, QIcon.Off)
+        btn.setIcon(icon)
+        btn.setIconSize(QSize(16, 16))
+        self.addBtnVGrid.addWidget(btn)
+        margin = QSpacerItem(5, 6, QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.addBtnVGrid.addItem(margin)
+        self.poleListing[idx]['add'] = btn
+
+    def addPoleName(self, row, idx, value):
         field = QLineEdit(self.tabPoles)
-        field.setMinimumSize(QSize(170, 0))
-        self.gridPoles.addWidget(field, idx + 1, 0, 1, 1)
+        field.setFixedWidth(180)        # TODO: Soltle wachsen können
         field.setText(value)
         self.poleListing[idx]['name'] = field
-    
-    def addPoleHDist(self, idx, value, valRange):
+        row.addWidget(field)
+
+    def addPoleHDist(self, row, idx, value, valRange):
         field = QDoubleSpinBox(self.tabPoles)
         field.setDecimals(1)
         field.setSingleStep(0.5)
         field.setSuffix(" m")
-        field.setMinimumSize(QSize(100, 0))
-        self.gridPoles.addWidget(field, idx + 1, 1, 1, 1)
+        field.setFixedWidth(95)
         field.setRange(float(valRange[0]), float(valRange[1]))  # TODO Range ist von der vorherigen und nachherigen Stütze abhängig
         field.setValue(float(value))
+        row.addWidget(field)
         self.poleListing[idx]['dist'] = field
-    
-    def addPoleHeight(self, idx, value):
+
+    def addPoleHeight(self, row, idx, value):
         field = QDoubleSpinBox(self.tabPoles)
         field.setDecimals(1)
         field.setSingleStep(0.1)
         field.setSuffix(" m")
-        field.setMinimumSize(QSize(100, 0))
-        self.gridPoles.addWidget(field, idx + 1, 2, 1, 1)
+        field.setFixedWidth(85)
         field.setRange(0.0, 50.0)
         field.setValue(float(value))
+        row.addWidget(field)
         self.poleListing[idx]['height'] = field
-    
-    def addPoleAngle(self, idx, value):
+
+    def addPoleAngle(self, row, idx, value):
         field = QSpinBox(self.tabPoles)
         field.setSuffix(" °")
-        field.setMinimumSize(QSize(79, 0))
-        self.gridPoles.addWidget(field, idx + 1, 3, 1, 1)
+        field.setFixedWidth(60)
         field.setRange(-180, 180)
         field.setValue(value)
+        row.addWidget(field)
         self.poleListing[idx]['angle'] = field
-    
-    def addPoleDel(self, idx):
-        field = QPushButton(self.tabPoles)
-        field.setMaximumSize(QSize(27, 27))
-        icon1 = QIcon()
-        icon1.addPixmap(
+
+    def addPoleDel(self, row, idx):
+        btn = QPushButton(self.tabPoles)
+        btn.setMaximumSize(QSize(27, 27))
+        icon = QIcon()
+        icon.addPixmap(
             QPixmap(":/plugins/SeilaplanPlugin/gui/icons/icon_bin.png"),
             QIcon.Normal, QIcon.Off)
-        field.setIcon(icon1)
-        field.setIconSize(QSize(16, 16))
-        self.gridPoles.addWidget(field, idx + 1, 4, 1, 1)
-        self.poleListing[idx]['del'] = field
+        btn.setIcon(icon)
+        btn.setIconSize(QSize(16, 16))
+        row.addWidget(btn)
+        self.poleListing[idx]['del'] = btn
     
     def Apply(self):
         self.close()
