425d258454d3450f6126cbccc150dcff9721e8a6
Pi Moll <pi1402@gmail.com>
2019-10-26 14:47:07 +0200

    Revert "Error display in progress window and better text layout."
    This reverts commit 73677d97

--- gui/adjustmentDialog.py
+++ gui/adjustmentDialog.py
@@ -23,8 +23,8 @@ import os
 import numpy as np
 from math import floor
 
-from qgis.PyQt.QtCore import QTimer
-from qgis.PyQt.QtWidgets import QDialog, QMessageBox
+from qgis.PyQt.QtCore import QSize, QTimer
+from qgis.PyQt.QtWidgets import QDialog, QSizePolicy, QMessageBox
 from qgis.PyQt.QtGui import QPixmap
 
 from .ui_adjustmentDialog import Ui_AdjustmenDialog
@@ -53,7 +53,9 @@ class AdjustmentDialog(QDialog, Ui_AdjustmenDialog):
         :type confHandler: configHandler.ConfigHandler
         """
         QDialog.__init__(self, interface.mainWindow())
+
         self.iface = interface
+        self.canvas = self.iface.mapCanvas()
         
         # Management of Parameters and settings
         self.confHandler = confHandler
@@ -74,11 +76,15 @@ class AdjustmentDialog(QDialog, Ui_AdjustmenDialog):
         # Setup GUI from UI-file
         self.setupUi(self)
 
-        self.drawTool = MapMarkerTool(self.iface.mapCanvas())
+        self.drawTool = MapMarkerTool(self.canvas)
         
-        # Create plot
+        # Create diagram
         self.plot = AdjustmentPlot(self)
-        # Pan/Zoom tools for plot
+        self.plot.setMinimumSize(QSize(600, 400))
+        self.plot.setMaximumSize(QSize(600, 400))
+        self.plot.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        
+        # Pan/Zoom tools for diagram
         bar = MyNavigationToolbar(self.plot, self)
         self.plotLayout.addWidget(self.plot)
         self.plotLayout.addWidget(bar)
@@ -101,7 +107,7 @@ class AdjustmentDialog(QDialog, Ui_AdjustmenDialog):
         self.unsavedChanges = True
 
         # Save dialog
-        self.saveDialog = DialogOutputOptions(self, self.confHandler)
+        self.saveDialog = DialogOutputOptions(self.iface, self, self.confHandler)
         
         # Connect signals
         self.btnClose.clicked.connect(self.onClose)
@@ -508,12 +514,3 @@ class AdjustmentDialog(QDialog, Ui_AdjustmenDialog):
             else:
                 self.drawTool.reset()
                 event.accept()
-
-    def resizeEvent(self, event):
-        newWidth = event.size().width()
-        oldHeight = self.plot.size().height()
-        # self.plot.resize(1000, 700)
-        # self.plot.resizePlot()
-        self.plot.resize(newWidth, oldHeight)
-        # print(event.size())
-        # print(self.plotLayout.sizeHint())
