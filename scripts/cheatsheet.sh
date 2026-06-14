5aa2ec08704fa435700052c68524f1fc1bbbc349
Pi Moll <patricia@opengis.ch>
2026-06-14 16:58:40 +0200

    Fix script to find translation strings

--- scripts/cheatsheet.sh
+++ scripts/cheatsheet.sh
@@ -12,7 +12,8 @@
 # When opening the ui file in the QtDesigner, resources have to be selected again.
 
 # get new translation texts
-bash scripts/update-strings.sh SeilaplanPlugin_de SeilaplanPlugin_en SeilaplanPlugin_fr SeilaplanPlugin_it
+cd SEILAPLAN
+bash i18n/update-strings.sh SeilaplanPlugin_de SeilaplanPlugin_en SeilaplanPlugin_fr SeilaplanPlugin_it
 ## This does not work properly, dont use it!
 ## The script will set some translations to "obsolete" because it can't
 ##  find the tr() calls in the code. Also, it will remove all translator
@@ -21,6 +22,7 @@ bash scripts/update-strings.sh SeilaplanPlugin_de SeilaplanPlugin_en SeilaplanPl
 # compile translations
 # Currently, this needs a conda env with Python 3.9, since the ubuntu 24.04 
 #  system python is at 3.12 and there are no pyqt6 tools currently for this version
-lrelease SeilaplanPlugin_de.ts SeilaplanPlugin_en.ts SeilaplanPlugin_fr.ts SeilaplanPlugin_it.ts
+cd SEILAPLAN
+lrelease i18n/SeilaplanPlugin_de.ts i18n/SeilaplanPlugin_en.ts i18n/SeilaplanPlugin_fr.ts i18n/SeilaplanPlugin_it.ts
 
 
